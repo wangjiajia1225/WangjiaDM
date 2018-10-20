59d71570a8f5e1e91c5ef288ee0a4aef13197716
wangjiajia1225 <1161733163@qq.com>
2018-10-21 01:38:35 +0800

    first

---
+++ test.c
@@ -0,0 +1,44 @@
+@@ -1,3 +1,4 @@
+
+
+ 
+
+
+/*this is a test file*/ 
+
+
+ 
+
+
+/* 
+ 
+@@ -10,7 +11,8 @@ manson ye
+
+
+#include<string.h> 
+
+
+ 
+
+
+ 
+
+
+int main() {	//main method 
+
+
+int main() //main method 
+
+
+{	 
+
+
+	int i = 100 / 4; 
+
+
+	 
+
+
+	printf("hello world");/**/ 
+ 
+
